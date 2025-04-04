d4224e0718720e9832e63af73a973991eb62fc6c
Dawid <ostrydawi9d1@gmail.com>
2025-04-04 16:48:14 +0200

    universal shooting target not specified slave 1,2,3

--- src/main.cpp
+++ src/main.cpp
@@ -12,27 +12,28 @@
 #define temperatureCelsius
 
 //BLE server name
-#define bleServerName1 "SLAVE_1"
+#define bleServerName1 "SHOOTING TARGET"
 #define bleServerName2 "SLAVE_2"
 #define bleServerName3 "SLAVE_3"
 
-int sensorNumber = 2;
+int sensorNumber = 1;
 
 float temp;
 
 bool deviceConnected = false;
 
-const int activeLed = 2;
-const int connectedLed = 26;
+const int ledBlue = 17;
+const int ledGreen = 18;
+const int ledRed = 2;
 const int wifiButton = 15;  // New button pin definition
 
 PiezoSensor sensor(34, 400);
 
 
-#define SERVICE_UUID1 "91bad492-b950-4226-aa2b-4ede9fa42f59"
+#define SERVICE_UUID1 "91bad492-b950-4226-aa2b-4ede9fa42fff"
 #define SERVICE_UUID2 "91bad492-b950-4226-aa2b-4ede9fa42f51"
 #define SERVICE_UUID3 "91bad492-b950-4226-aa2b-4ede9fa42333"
-#define CHARACTERISTIC1 "cba1d466-344c-4be3-ab3f-189f80dd7518"
+#define CHARACTERISTIC1 "cba1d466-344c-4be3-ab3f-189f80dd75ff"
 #define CHARACTERISTIC2 "cba1d466-344c-4be3-ab3f-189f80dd7511"
 #define CHARACTERISTIC3 "cba1d466-344c-4be3-ab3f-189f80dd7333"
 
@@ -44,7 +45,10 @@ BLEDescriptor *piezoDescriptor;
 BLEService *piezoService;
 BLEServer *pServer;
 
-
+// Add these constants at the top with other definitions
+const int LONG_PRESS_TIME = 3000;  // 3 seconds for long press
+const int SHORT_PRESS_TIME = 50;   // 50ms debounce time for short press
+bool isInDeepSleep = false;
 
 class WriteCallbacks: public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic *pCharacteristic) {
@@ -53,7 +57,9 @@ class WriteCallbacks: public BLECharacteristicCallbacks {
     Serial.println(value.c_str());
     // Handle the received value
     if (value == "start") {
-      digitalWrite(connectedLed, LOW);
+      digitalWrite(ledRed, HIGH);
+      digitalWrite(ledGreen, HIGH);
+      digitalWrite(ledBlue, HIGH);
     }
   }
 };
@@ -63,20 +69,18 @@ class WriteCallbacks: public BLECharacteristicCallbacks {
 //Setup callbacks onConnect and onDisconnect
 class MyServerCallbacks: public BLEServerCallbacks {
   void onConnect(BLEServer* pServer) {
+    Serial.println("Device connected");
     deviceConnected = true;
-    digitalWrite(connectedLed, HIGH); 
+    for(int i = 0; i < 3; i++){
+    digitalWrite(ledRed, HIGH); 
+    digitalWrite(ledGreen, HIGH);
+    digitalWrite(ledBlue, HIGH);
     delay(200);
-    digitalWrite(connectedLed, LOW);
+    digitalWrite(ledRed, LOW);
+    digitalWrite(ledGreen, LOW);
+    digitalWrite(ledBlue, LOW);
     delay(200);
-    digitalWrite(connectedLed, HIGH);
-    delay(200);
-    digitalWrite(connectedLed, LOW);
-    delay(200);
-    digitalWrite(connectedLed, HIGH);
-    delay(200);
-    digitalWrite(connectedLed, LOW);
-    delay(200);
-    digitalWrite(connectedLed, HIGH);
+    }
   };
   void onDisconnect(BLEServer* pServer) {
     deviceConnected = false;
@@ -85,7 +89,9 @@ class MyServerCallbacks: public BLEServerCallbacks {
 };
 
 void ledOff(int piezoValue) {
-  digitalWrite(connectedLed, HIGH); 
+  digitalWrite(ledRed, LOW);
+  digitalWrite(ledGreen, LOW);
+  digitalWrite(ledBlue, LOW);
   if (deviceConnected) {
   
     String valueToSend = String(piezoValue); // Convert to String
@@ -99,16 +105,27 @@ void ledOff(int piezoValue) {
       
 }
 
+void goToDeepSleep() {
+  Serial.println("Going to deep sleep...");
+ 
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_15, 0);
+  
+  // Go to sleep
+  esp_deep_sleep_start();
+}
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Start");
   Serial.print("device number: ");
   Serial.println(sensorNumber);
 
-  pinMode(activeLed, OUTPUT);
-  digitalWrite(activeLed, LOW); 
-  pinMode(connectedLed, OUTPUT);
-  digitalWrite(connectedLed, HIGH); 
+  pinMode(ledBlue, OUTPUT);
+  digitalWrite(ledBlue, LOW);
+  pinMode(ledGreen, OUTPUT);
+  digitalWrite(ledGreen, LOW);
+  pinMode(ledRed, OUTPUT);
+  digitalWrite(ledRed, LOW); 
   pinMode(wifiButton, INPUT_PULLUP);  // Initialize button pin
 
   sensor.begin();
@@ -127,71 +144,113 @@ void setup() {
     Serial.print("Initialized as SLAVE_3 ");
   }
 
-  // // Initialize BLE Device with the device name
-  // BLEDevice::init(deviceName.c_str());
+  // Initialize BLE Device with the device name
+  BLEDevice::init(deviceName.c_str());
 
-  // // Create the BLE Server
-  // pServer = BLEDevice::createServer();
-  // pServer->setCallbacks(new MyServerCallbacks());
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
 
-  // // Create the BLE Service and Characteristic
-  // if(sensorNumber == 1){
-  //   piezoService = pServer->createService(SERVICE_UUID1);
-  //   piezoCharacteristic = piezoService->createCharacteristic(
-  //                           CHARACTERISTIC1,
-  //                           BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE
-  //                         );
-  // } else if (sensorNumber == 2){
-  //   piezoService = pServer->createService(SERVICE_UUID2);
-  //   piezoCharacteristic = piezoService->createCharacteristic(
-  //                           CHARACTERISTIC2,
-  //                           BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE
-  //                         );
-  // } else if (sensorNumber == 3){
-  //   piezoService = pServer->createService(SERVICE_UUID3);
-  //   piezoCharacteristic = piezoService->createCharacteristic(
-  //                           CHARACTERISTIC3,
-  //                           BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE
-  //                         );
-  // }
+  // Create the BLE Service and Characteristic
+  if(sensorNumber == 1){
+    piezoService = pServer->createService(SERVICE_UUID1);
+    piezoCharacteristic = piezoService->createCharacteristic(
+                            CHARACTERISTIC1,
+                            BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE
+                          );
+  } else if (sensorNumber == 2){
+    piezoService = pServer->createService(SERVICE_UUID2);
+    piezoCharacteristic = piezoService->createCharacteristic(
+                            CHARACTERISTIC2,
+                            BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE
+                          );
+  } else if (sensorNumber == 3){
+    piezoService = pServer->createService(SERVICE_UUID3);
+    piezoCharacteristic = piezoService->createCharacteristic(
+                            CHARACTERISTIC3,
+                            BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE
+                          );
+  }
 
-  // // Add Descriptor and Callbacks
-  // piezoCharacteristic->addDescriptor(new BLE2902());
-  // piezoCharacteristic->setCallbacks(new WriteCallbacks());
+  // Add Descriptor and Callbacks
+  piezoCharacteristic->addDescriptor(new BLE2902());
+  piezoCharacteristic->setCallbacks(new WriteCallbacks());
 
-  // // Start the service
-  // piezoService->start();
+  // Start the service
+  piezoService->start();
 
-  // // Configure the advertising data
-  // BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  // pAdvertising->addServiceUUID(piezoService->getUUID());
-  // pAdvertising->setScanResponse(true);
+  // Configure the advertising data
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(piezoService->getUUID());
+  pAdvertising->setScanResponse(true);
 
-  // // Explicitly set the device name in the advertising data
-  // BLEAdvertisementData advertisementData;
-  // advertisementData.setName(deviceName.c_str());
-  // pAdvertising->setAdvertisementData(advertisementData);
+  // Explicitly set the device name in the advertising data
+  BLEAdvertisementData advertisementData;
+  advertisementData.setName(deviceName.c_str());
+  pAdvertising->setAdvertisementData(advertisementData);
 
-  // // Start advertising
-  // BLEDevice::startAdvertising();
+  // Start advertising
+  BLEDevice::startAdvertising();
 
-  // Serial.println("Waiting for a client connection to notify...");
+  Serial.println("Waiting for a client connection to notify...");
+
+  // After ESP32 wakes up from deep sleep
+  if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0) {
+    Serial.println("Woken up from deep sleep");
+  }
 }
 
 void loop() {
-  // Check if WiFi button is pressed
-  if (digitalRead(wifiButton) == LOW) {
-    Serial.println("WiFi button pressed, starting WiFiManager portal");
-    WiFiManager wifiManager;
-    wifiManager.startConfigPortal("ShootingSystemAP");  // Name of the config AP
+  static unsigned long pressStartTime = 0;
+  static bool buttonPressed = false;
+  
+  // Button handling
+  if (digitalRead(wifiButton) == LOW) {  // Button is pressed
+    if (!buttonPressed) {  // Button was just pressed
+      pressStartTime = millis();
+      buttonPressed = true;
+      Serial.println("Button pressed");
+    }
+    
+    // Check for long press while button is still held
+    if ((millis() - pressStartTime) > LONG_PRESS_TIME) {
+      Serial.println("Long press detected");
+       for(int i = 0; i < 3; i++){
+  digitalWrite(ledBlue, LOW);
+  digitalWrite(ledGreen, LOW);
+  digitalWrite(ledRed, LOW);
+  delay(200);
+  digitalWrite(ledBlue, HIGH);
+  digitalWrite(ledGreen, HIGH);
+  digitalWrite(ledRed, HIGH);
+  delay(200);
+  }
+  
+      vTaskDelay(3000 / portTICK_PERIOD_MS);
+      goToDeepSleep();
+    }
+  }
+  else if (buttonPressed) {  // Button was released
+    if ((millis() - pressStartTime) > SHORT_PRESS_TIME &&
+        (millis() - pressStartTime) < LONG_PRESS_TIME) {
+      // Short press detected
+      Serial.println("WiFi button pressed, starting WiFiManager portal");
+      WiFiManager wifiManager;
+      wifiManager.startConfigPortal("ShootingSystemAP");
+    }
+    buttonPressed = false;
   }
 
-  // if (!deviceConnected) {
-  //   digitalWrite(connectedLed, LOW); 
-  //   delay(500);
-  //   digitalWrite(connectedLed, HIGH);
-  
-  //   delay(500);
-  // }
-   delay(100);
+  if (!deviceConnected) {
+    Serial.println("Device not connected");
+    digitalWrite(ledRed, LOW); 
+    digitalWrite(ledGreen, LOW);
+    digitalWrite(ledBlue, LOW);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    digitalWrite(ledRed, HIGH);
+    digitalWrite(ledGreen, HIGH);
+    digitalWrite(ledBlue, HIGH);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+   
 }
